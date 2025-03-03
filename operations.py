b3000ce7534121db0160776604a35d11c1eedc89
Chocissa315 <150431999+Chocissa315@users.noreply.github.com>
2025-03-03 10:40:40 +0100

    Creation of a function to determine if an automata is standard or not

--- operations.py
+++ operations.py
@@ -5,6 +5,7 @@
 # Created:     01/03/2025
 # ---------------------------------------------------------------------------------------------------------------
 # IMPORTATIONS OF MODULES
+from automata import FiniteAutomaton
 
 
 '''
@@ -13,7 +14,56 @@
  * @return SFA: The standardized automaton
  '''
 def standardization(FA):
-    pass
+       pass
+
+def is_standard(filename :str) :
+
+    fa = FiniteAutomaton()
+    fa.read_automaton_from_file(filename)
+    standard = True           # we begin by saying that the automata is standard
+
+    with open(filename, "r") as file:
+        lines = file.readlines()  # read the entire file and store it in a list of strings
+        first_lines = lines[:4]  # retrieve the first 4 lines as they contain
+        # the general information of the automaton | use readlines() as the first 4 lines are always the same
+
+    next_state=[]
+
+    initial_states_line = first_lines[2].split()  # split the line into a list of strings
+    nb_initial_states = initial_states_line[0].strip()  # retrieve the number of initial states
+    initial_states = initial_states_line[1:]
+
+    transitions = fa.transitions
+
+    for states in transitions :
+        third_char = states[2:]
+        next_state.append(third_char)
+
+
+    if int(nb_initial_states) > 1 :
+        standard = False  #If there is more than one initial state, the automata is not standard
+
+    for i in next_state :
+        for j in initial_states:
+            if i == j :
+                standard = False
+
+    return standard
+
+
+
+filename = "Automatons/project_automaton_test.txt"
+standard = is_standard(filename)
+print(standard)
+
+
+
+
+
+
+
+
+
 
 
 '''
@@ -50,3 +100,4 @@ def minimization(CDFA):
  '''
 def complementary_automaton(A):
     pass
+
