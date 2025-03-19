151315a54e17c640eee2471f410278e12b8e591c
YannisBAHHH <150543425+YannisBAHHH@users.noreply.github.com>
2025-03-19 09:01:02 +0100

    Minimization function

--- main.py
+++ main.py
@@ -9,21 +9,14 @@ import automata
 import operations
 import properties_check
 
-user_automaton = int(input("Enter the number corresponding to the automaton you want to manipulate: "))
+user_automaton = input("Enter the number corresponding to the automaton you want to manipulate: ")
 
-if user_automaton == 1:
-    test = automata.FiniteAutomaton()
-    test.read_automaton_from_file("Automatons/project_automaton_test.txt")
-    test.display_automaton()
-elif user_automaton == 2:
-    test2 = automata.FiniteAutomaton()
-    test2.read_automaton_from_file("Automatons/project_automaton_test_2")
-    test2.display_automaton()
-elif user_automaton == 3:
-    test3 = automata.FiniteAutomaton()
-    test3.read_automaton_from_file("Automatons/automaton_20.txt")
-    test3.display_automaton()
-elif user_automaton == 4:
-    test4 = automata.FiniteAutomaton()
-    test4.read_automaton_from_file("Automatons/automaton_27.txt")
-    test4.display_automaton()
+
+
+test = automata.FiniteAutomaton()
+test.read_automaton_from_file(f"Automatons/automaton_{user_automaton}.txt")
+test.display_automaton()
+
+minimal_auto = operations.minimization2(test)
+
+minimal_auto.display_automaton()
--- operations.py
+++ operations.py
@@ -44,7 +44,7 @@ def completion(FA):
                 # once we made sure a bin exist, we add the missing transition towards the bin.
                 FA.transitions[(state, symbol)] = "P"
     return FA
-    pass
+
 
 
 # this function isn't great because it only works on standardized FA
@@ -66,9 +66,77 @@ def determinization_and_completion_of_automaton(FA):
  * @return MCDFA: The minimized CDFA
  '''
 
+def minimization2(CDFA):
 
-def minimization(CDFA):
-    pass
+    MCDFA = automata.FiniteAutomaton()
+    MCDFA.alphabet = CDFA.alphabet
+
+    current_partitioning = [] # list of lists of states representing the groups
+    current_partitioning.append(CDFA.terminal_states)
+    current_partitioning.append([state for state in CDFA.states if state not in CDFA.terminal_states]) # Non-terminal states
+
+    minimized = 0
+    while not minimized :
+        patterns = [] 
+        # patterns will contain the pattern for each state. The indexes are equal to the names of the state, the pattern of the state named '0' is at the index 0.
+        # At the end, we make new groups, joining the states having the same patterns
+
+        for i in range(len(CDFA.states)):
+
+            this_pattern = []   # the pattern of the current state
+            for char in CDFA.alphabet :
+                for j in range(len(current_partitioning)):
+                    if next(iter(CDFA.transitions[(CDFA.states[i], char)])) in current_partitioning[j] :
+                        this_pattern.append(j)
+
+            this_pattern.append(CDFA.states[i] in CDFA.terminal_states) # 0 or 1 for terminal or not
+
+            patterns.append(tuple(this_pattern))
+
+        next_partitioning = []
+        for pattern in patterns:
+            new_group = [i for i, p in enumerate(patterns) if p == pattern]
+            if new_group not in next_partitioning :
+                next_partitioning.append(new_group)
+        
+        if len(current_partitioning) == len(next_partitioning): # If the number of groups is still the same, it means no group have been created, so we're done
+            minimized = 1
+        else :
+            current_partitioning = next_partitioning
+    # end of while
+
+    # We now have found the minimal automaton, we have to build it
+
+    MCDFA.states = [i for i in range(len(current_partitioning))]
+
+    MCDFA.initial_states = [i for i in MCDFA.states if CDFA.initial_states[0] in current_partitioning[i]]
+
+
+    MCDFA.terminal_states = []
+
+    for i in MCDFA.states :
+        for t in CDFA.terminal_states :
+            if i not in MCDFA.terminal_states :
+                if t in current_partitioning[i] :
+                    MCDFA.terminal_states.append(i)
+
+
+    # Transitions part
+    MCDFA.transitions = {}
+
+    for key, value in CDFA.transitions.items():
+
+        # dermining the corresponding state
+        for i in MCDFA.states :
+            if key[0] in current_partitioning[i]:
+                starting_state = i
+            if next(iter(value)) in current_partitioning[i] :   # value is a set of 1 element, because CDFA is deterministic
+                arriving_state = i
+        
+        if (starting_state, key[1]) not in MCDFA.transitions :
+            MCDFA.transitions[(starting_state, key[1])] = {arriving_state}
+    
+    return MCDFA
 
 
 '''
