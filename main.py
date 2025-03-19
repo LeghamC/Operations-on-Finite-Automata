4832494322015d862e0a01e11127a252219b19da
Lipotani77 <kkoulourath@icloud.com>
2025-03-19 18:02:00 +0100

    oe

--- main.py
+++ main.py
@@ -10,28 +10,11 @@ import operations
 import properties_check
 
 
-user_automaton = input("Enter the number corresponding to the automaton you want to manipulate: ")
+# user_automaton = input("Enter the number corresponding to the automaton you want to manipulate: ")
 test = automata.FiniteAutomaton()
 test.read_automaton_from_file(f"Automatons/automaton_{user_automaton}.txt")
 test.display_automaton()
 
-operations.is_standard(test)
-if operations.is_standard(test) == False :
-    operations.standardization(test)
-test.display_automaton()
 
-
-test2 = automata.FiniteAutomaton()
-test2.read_automaton_from_file("Automatons/project_automaton_test_2")
-test2.display_automaton()
-
-operations.is_standard(test2)
-if operations.is_standard(test2) == False :
-    operations.standardization(test2)
-test2.display_automaton()
-
-test_automaton = automata.FiniteAutomaton()  # create an automaton
-test_automaton.read_automaton_from_file("Automatons/project_automaton_test.txt")  # read the automaton from a file
-test_automaton.display_automaton()
-for transition in test_automaton.transitions:
+for transition in test.transitions:
     print(transition)
