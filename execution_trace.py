360a7594ca3fc8e059b35d2d1322eef1a6775023
Lipotani77 <kkoulourath@icloud.com>
2025-03-22 19:57:36 +0100

    the execution traces function is DOOOONE

--- execution_trace.py
+++ execution_trace.py
@@ -2,11 +2,13 @@ import automata
 import operations
 import properties_check
 import general_functions
+import word_recognition as WR
 
 
-def get_execution_trace(num_automaton: int, file_name: str):
+def get_execution_trace(num_automaton: int, word_to_test: str, file_name: str):
     """
 
+    :param word_to_test: the word you want to test
     :param num_automaton : the number of the corresponding automaton you want to get the trace
     :param file_name: the file where you want to get the execution trace
     :return: nothing
@@ -16,37 +18,129 @@ def get_execution_trace(num_automaton: int, file_name: str):
     FA = automata.FiniteAutomaton()
     FA.read_automaton_from_file(f"Automatons/Automaton_{num_automaton}.txt")
 
+    menu_str = general_functions.display_menu()  # get the menu in a variable, it contains the exact one as the
+    # one showing on the terminal
+    mid_menu_str = general_functions.display_mid_menu()
+
     with open(file_name, "w", encoding="utf-8") as trace:
-        menu_str = general_functions.display_menu()  # get the menu in a variable, it contains the exact one as the
-        # one showing on the
-        # terminal
-
         trace.write(menu_str + "\n")
-        trace.write("[TERMINAL] Enter the number corresponding to the operation you want to do: \n")
-        trace.write("[USER] " + str(1) + "\n")
-        trace.write("\n")
-        trace.write("[TERMINAL] Enter the number corresponding to the automaton you want to manipulate: \n")
+        trace.write("[TERMINAL] Enter the number corresponding to the automaton you want to manipulate or type -1 to "
+                    "exit: \n")
         trace.write("[USER] " + str(num_automaton) + "\n")
-        trace.write(general_functions.display_mid_menu() + 2 * "\n")
-        trace.write("[USER] I chose to display the automaton\n")
+        trace.write("\n")
         trace.write("[TERMINAL] Here is the display of the automaton you are manipulating: \n")
-        if properties_check.is_asynchronous(FA):
-            trace.write(
-                "[TERMINAL] The automaton is asynchronous it cannot be shown right now, it should be determined "
-                "before\n")
-            trace.write("[TERMINAL] Your automaton has been determined in order to display it\n")
-    # Careful here, we have to check for asynchronous automaton as they cannot be displayed, it needs to be
-    # determined first
-    if properties_check.is_asynchronous(FA):
-        new_FA = operations.determinization_asynchronous(FA)
-        new_FA.display_automaton_redirected(file_name)
-    else:
-        FA.display_automaton_redirected(file_name)
-
-    with open(file_name, "a", encoding="utf-8") as trace:
-        trace.write(menu_str + "\n")
-
+    FA.display_automaton_redirected(file_name)
     trace.close()
 
+    # -------------- Standardization ----------------------------------
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write(mid_menu_str + "\n")
+        trace.write("[USER] 1\n")
+        if properties_check.is_standard(FA):
+            print("\nThe automaton is already standard.")
+    if not properties_check.is_standard(FA):
+        FA = operations.standardization(FA)
+    trace.close()
 
-get_execution_trace(10, "Automatons/test.txt")
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write("[TERMINAL] The standardized version of the automaton is the following: \n")
+    FA.display_automaton_redirected(file_name)
+    trace.close()
+    # ------------------------------------------------------------------
+
+    # ---------------- Determinization and completion -------------------
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write(
+            "[TERMINAL] Displays the menu again in order for the user to choose the operation he wants to do: \n")
+        trace.write("[USER] 2" + "\n")
+        trace.write("[BACKGROUND] Checks if the automaton is asynchronous or not as we have different function "
+                    "depending on that\n")
+        trace.write("PUT HERE THE PART WHEN WE DECIDE WHAT TO DO WITH THE DERTERMINIZATION\n\n")
+    trace.close()
+    # ------------------------------------------------------------------
+
+    # # ------ Minimization part ---------------------------------------
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write(
+            "[TERMINAL] Displays the menu again in order for the user to choose the operation he wants to do: \n")
+        trace.write("[USER] 3" + "\n")
+
+    deterministic_conditions = properties_check.is_deterministic(FA)
+    complete = properties_check.is_complete(FA)
+
+    if all(condition == 1 for condition in deterministic_conditions) and complete == 1:
+        MCDFA = operations.minimization(FA)
+        MCDFA.display_automaton_redirected(file_name)
+    elif all(condition == 1 for condition in deterministic_conditions) and complete == 0:
+        with open(file_name, "a", encoding="utf-8") as trace:
+            trace.write("The automaton you entered was deterministic but not complete."
+                        "Hence, we completed it before minimizing it."
+                        "\nThe minimized automaton is the following: \n")
+        CDFA = operations.determinization_and_completion_automaton(FA)
+        MCDFA = operations.minimization(CDFA)
+        MCDFA.display_automaton_redirected(file_name)
+
+    else:
+        with open(file_name, "a", encoding="utf-8") as trace:
+            trace.write(
+                "The automaton you entered was not deterministic. Hence, we determinized and if needed completed"
+                "it before minimizing it."
+                "\n The minimized automaton is the following: \n")
+        CDFA = operations.determinization_and_completion_automaton(FA)
+        MCDFA = operations.minimization(CDFA)
+        MCDFA.display_automaton_redirected(file_name)
+
+    trace.close()
+    # --------------------------------------
+
+    # -------------Word recognition-----------------------------
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write(
+            "\n[TERMINAL] Displays the menu again in order for the user to choose the operation he wants to do: \n")
+        trace.write("[USER] 4" + "\n")
+        trace.write("[TERMINAL] Enter a word you want to read : \n")
+        trace.write("[USER] " + word_to_test + "\n")
+    if WR.recognize_word(word_to_test, FA):
+        with open(file_name, "a", encoding="utf-8") as trace:
+            trace.write(f"[TERMINAL] The automaton recognizes the word '{word_to_test}'.\n")
+    else:
+        with open(file_name, "a", encoding="utf-8") as trace:
+            trace.write(f"[TERMINAL] The automaton does not recognize the word '{word_to_test}'.\n")
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write("\nATTENTION YOU HAVE TO COPY WHAT IS RIGHT AFTER AND PASTE IT AFTER THE USER 5\n")
+    trace.close()
+    # ----------------------------------------------------------
+
+    # -------------Word recognition-----------------------------
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write(
+            "\n[TERMINAL] Displays the menu again in order for the user to choose the operation he wants to do: \n")
+        trace.write("[USER] 5" + "\n")
+        with open(file_name, "a", encoding="utf-8") as trace:
+            trace.write("The automaton recognizing the complementary language of the current automaton is the "
+                        "following: \n")
+        CFA = operations.complementary_automaton(FA)
+        CFA.display_automaton_redirected(file_name)
+    trace.close()
+    # ----------------------------------------------------------
+
+    # ----------------Word recognition of the complementary language --------------------------------------
+    with open(file_name, "a", encoding="utf-8") as trace:
+        trace.write(
+            "\n[TERMINAL] Displays the menu again in order for the user to choose the operation he wants to do: \n")
+        trace.write("[USER] 6" + "\n")
+        trace.write("[TERMINAL] Enter a word you want to read : \n")
+
+    CFA = operations.complementary_automaton(FA)
+    if WR.recognize_word(word_to_test, CFA):
+        with open(file_name, "a", encoding="utf-8") as trace:
+            trace.write(f"The automaton recognizing the complementary language of the current automaton recognizes the"
+                  f"word '{word_to_test}'.\n'.")
+    else:
+        with open(file_name, "a", encoding="utf-8") as trace:
+            trace.write(f"The automaton recognizing the complementary language of the current automaton does not "
+                  f"recognize the word '{word_to_test}'.\n'.")
+    # -------------------------------------- -----------------------------------------------------------
+
+
+get_execution_trace(1, "", "Automatons/test.txt")
